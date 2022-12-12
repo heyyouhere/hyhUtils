9d2a90e7b84a5ed7ec275d8309de14362c25e07e
Maxim Kalanchekaev <45402247+heyyouhere@users.noreply.github.com>
2022-12-12 18:58:19 +0300

    Added Vector3Projection

--- RobloxUtils.lua
+++ RobloxUtils.lua
@@ -9,3 +9,14 @@ function secondsToTime(seconds)
 	end
 	return {minuteString, secondsString}
 end
+
+
+function vector3Projection(vector_A, vector_B)
+	local result = 0
+	local scalar = vector_A.X * vector_B.X + vector_A.Y * vector_B.Y + vector_A.Z + vector_B.Z
+	local vectorB_len = math.sqrt(vector_B.X * vector_B.X + vector_B.Y * vector_B.Y + vector_B.Z * vector_B.Z)
+	if vectorB_len ~= 0 then
+		result = scalar / vectorB_len
+	end
+	return result
+end
